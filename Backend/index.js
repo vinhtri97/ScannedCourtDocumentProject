bdda5e855513c6ff605a9b93c33853d283a9a10e
Tri Le <letranvinhtri@gmail.com>
2019-02-26 12:29:17 -0600

    Initialize Project

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+//convert PDF first page to img
+app.get('/RecentlyPDF', (req,res) => {
+    const path = require('path');
+    const pdf = require('pdf-poppler');
+    let file = "..\\Databases\\Civil\\testfile.pdf";
+
+    let opts = {
+      format: 'jpeg',
+      out_dir: path.dirname("..\\Databases\\RecentlyVisitPDF\\testfile"),
+      out_prefix: path.basename(file, path.extname(file)),
+      page: null
+    }
+    pdf.convert(file, opts)
+    .then(pdfInfo => {
+        res.sendFile("D:\\TAMUCC\\Spring2019\\SeniorCapstone\\Project\\Databases\\RecentlyVisitPDF\\testfile-1.jpg");
+    })
+    .catch(error => {
+        console.error(error);
+    })
+});
+const port = 5000;
+app.listen(port,()=> console.log('Listening on port 5000 ....'));
